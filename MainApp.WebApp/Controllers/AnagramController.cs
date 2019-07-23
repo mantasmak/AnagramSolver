3e540f862a17c27cb5635f9773b14ebe08df5888
Mantas Maksimavicius <mantas@LT-LIT-SC-0009.internal.visma.com>
2019-07-23 17:40:48 +0300

    Add frontend, implement api.

---
+++ MainApp.WebApp/Controllers/AnagramController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Contracts;
+
+namespace MainApp.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnagramController : ControllerBase
+    {
+        IAnagramSolver anagramService;
+
+        public AnagramController(IAnagramSolver anagramService)
+        {
+            this.anagramService = anagramService;
+        }
+
+        // GET: api/Anagram?word=...
+        [HttpGet]
+        public IEnumerable<string> Get(string word)
+        {
+            string ip = HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+            return anagramService.GetAnagrams(word, ip);
+        }
+    }
+}
---
+++ MainApp.WebApp/Controllers/WordController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Contracts;
+
+namespace MainApp.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordController : ControllerBase
+    {
+        IWordService wordService;
+        IWordsManipulator wordsManipulationService;
+
+        public WordController(IWordService wordService, IWordsManipulator wordsManipulationService)
+        {
+            this.wordService = wordService;
+            this.wordsManipulationService = wordsManipulationService;
+        }
+
+        // GET: api/Word
+        [HttpGet("{startIndex}", Name = "Get")]
+        public IEnumerable<string> Get(int startIndex)
+        {
+            int pageSize = 100;
+
+            return wordService.GetAllWords()
+                .Skip(startIndex)
+                .Take(pageSize)
+                .ToList();
+        }
+
+        // POST: api/Word
+        [HttpPost]
+        public void Post(string value)
+        {
+            string ip = HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+            wordsManipulationService.AddWord(value, ip);
+        }
+
+        // PUT: api/Word/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
